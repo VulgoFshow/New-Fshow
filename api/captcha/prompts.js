8cedefcbbc5f9bcde71d75b47e78a660357bd428
VulgoFshow <contatofshow7@gmail.com>
2025-12-18 19:51:04 -0300

    Create prompts.js

---
+++ api/captcha/prompts.js
@@ -0,0 +1,35 @@
+export const CAPTCHA_PROMPTS = [
+  {
+    id: "locomocao",
+    label: "Clique nas imagens contendo algo que pode se locomover",
+    correctTag: "202",
+    images: [
+      { src: "https://i.ibb.co/cSMQWgx4/car1.png", tag: "202" },
+      { src: "https://i.ibb.co/d0CCf03S/moto1.png", tag: "202" },
+      { src: "https://i.ibb.co/tp8Mxrd9/car3.png", tag: "202" },
+      { src: "https://i.ibb.co/vtwFRhB/moto2.png", tag: "202" },
+      { src: "https://i.ibb.co/kVQVS7yD/car4.png", tag: "202" },
+      { src: "https://i.ibb.co/1f3FBvb9/montanha2.png", tag: "404" },
+      { src: "https://i.ibb.co/8LztFZt4/montanha.png", tag: "404" },
+      { src: "https://i.ibb.co/XZwwyFZq/arvore.png", tag: "404" },
+      { src: "https://i.ibb.co/ZDG821x/arvore2.png", tag: "404" },
+      { src: "https://thumbs.dreamstime.com/b/rocha-ou-pedra-grande-na-montanha-118007979.jpg", tag: "404" }
+    ]
+  },
+  {
+    id: "animais",
+    label: "Clique nas imagens contendo um animal",
+    correctTag: "706",
+    images: [
+      { src: "https://i.ibb.co/PZPW0gzR/elefante.jpg", tag: "706" },
+      { src: "https://i.ibb.co/zHZ6BhsR/cavalo.jpg", tag: "706" },
+      { src: "https://i.ibb.co/Nw9HKGp/leao.jpg", tag: "706" },
+      { src: "https://i.ibb.co/VcBzNJsw/rino.jpg", tag: "706" },
+      { src: "https://i.ibb.co/0jr3fwBd/girafa.jpg", tag: "706" },
+      { src: "https://i.ibb.co/WNh2CsLC/torta.png", tag: "1412" },
+      { src: "https://i.ibb.co/BHvGC4Cr/cookie.png", tag: "1412" },
+      { src: "https://i.ibb.co/35sfVQvY/donuts.png", tag: "1412" },
+      { src: "https://i.ibb.co/rRT6hz08/bolo.png", tag: "1412" }
+    ]
+  }
+];
