50b805c14117cd46827c841e51a94eb296453b1b
VulgoFshow <contatofshow7@gmail.com>
2026-07-16 20:59:54 -0300

    Delete api directory

--- api/test.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-  runtime: "nodejs"
-};
-
-module.exports = (req, res) => {
-  res.status(200).json({ ok: true });
-};
--- api/time.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    now: new Date().toISOString()
-  });
-}
