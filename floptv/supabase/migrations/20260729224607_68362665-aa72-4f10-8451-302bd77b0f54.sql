89cc3093380a421414bcf4e6c05c9ddded6e2f33
VulgoFshow <contatofshow7@gmail.com>
2026-07-31 12:45:00 -0300

    Delete floptv directory

--- floptv/supabase/migrations/20260729224607_68362665-aa72-4f10-8451-302bd77b0f54.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE public.stream_config
-  ADD COLUMN IF NOT EXISTS started_at timestamptz,
-  ADD COLUMN IF NOT EXISTS sync_mode boolean NOT NULL DEFAULT true,
-  ADD COLUMN IF NOT EXISTS loop_stream boolean NOT NULL DEFAULT true;
