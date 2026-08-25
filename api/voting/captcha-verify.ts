f7844ee5d6f75b8aac6b6200423c599b92050102
VulgoFshow <contatofshow7@gmail.com>
2026-08-25 11:19:17 -0300

    Delete api directory

--- api/voting/captcha-verify.ts
+++
@@ -1,5 +0,0 @@
-import { verifyCaptchaChallenge } from "../../lib/voting";
-
-export async function POST(request: Request) {
-  return verifyCaptchaChallenge(request);
-}
--- api/voting/vote.ts
+++
@@ -1,5 +0,0 @@
-import { submitVote } from "../../lib/voting";
-
-export async function POST(request: Request) {
-  return submitVote(request);
-}
