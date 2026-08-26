56fcda0df5706434ce527e0464f250afe9f1705f
VulgoFshow <contatofshow7@gmail.com>
2026-08-26 13:10:19 -0300

    Delete api/voting directory

--- api/voting/vote.ts
+++
@@ -1,9 +0,0 @@
-import { submitVote } from "../../lib/voting";
-
-export async function POST(request: Request) {
-  return submitVote(request);
-}
-
-export async function GET() {
-  return Response.json({ ok: false, error: "Não foi possível concluir a solicitação." }, { status: 405 });
-}
